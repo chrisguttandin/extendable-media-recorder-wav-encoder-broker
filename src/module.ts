4caefc3c9ef8ca102e2d18abb828040aac1baadb
chrisguttandin <chrisguttandin@media-codings.com>
2017-12-26 12:09:05 +0100

    replace deprecated ReflectiveInjector with Injector

--- src/module.ts
+++ src/module.ts
@@ -1,17 +1,17 @@
 import 'core-js/es7/reflect'; // tslint:disable-line:ordered-imports
-import { ReflectiveInjector } from '@angular/core'; // tslint:disable-line:ordered-imports
-import { WavEncoderFactory } from './factories/wav-encoder';
-import { WavRecorderFactory } from './factories/wav-recorder';
+import { Injector } from '@angular/core'; // tslint:disable-line:ordered-imports
+import { WAV_ENCODER_FACTORY_PROVIDER, WavEncoder, WavEncoderFactory } from './factories/wav-encoder';
+import { WAV_RECODER_FACTORY_PROVIDER } from './factories/wav-recorder';
 import { recordingIds } from './providers/recording-ids';
 import { unrespondedRequests } from './providers/unresponded-requests';
 import { worker } from './providers/worker';
 
-export const load = (url: string) => {
-    const injector = ReflectiveInjector.resolveAndCreate([
+export const load = (url: string): WavEncoder => {
+    const injector = Injector.create([
         { provide: recordingIds, useValue: new Set<number>() },
         { provide: unrespondedRequests, useValue: new Set<number>() },
-        WavEncoderFactory,
-        WavRecorderFactory,
+        WAV_ENCODER_FACTORY_PROVIDER,
+        WAV_RECODER_FACTORY_PROVIDER,
         { provide: worker, useValue: new Worker(url) }
     ]);
 
