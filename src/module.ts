fdbe5bcea684ddfd3e1bd18a93108ef8cbab0838
chrisguttandin <chrisguttandin@media-codings.com>
2018-01-11 03:14:05 +0100

    style: apply linting rules

--- src/module.ts
+++ src/module.ts
@@ -9,13 +9,15 @@ import { worker } from './providers/worker';
 export * from './interfaces';
 
 export const load = (url: string): IWavEncoder => {
-    const injector = Injector.create([
-        { provide: recordingIds, useValue: new Set<number>() },
-        { provide: unrespondedRequests, useValue: new Set<number>() },
-        WAV_ENCODER_FACTORY_PROVIDER,
-        WAV_RECODER_FACTORY_PROVIDER,
-        { provide: worker, useValue: new Worker(url) }
-    ]);
+    const injector = Injector.create({
+        providers: [
+            { provide: recordingIds, useValue: new Set<number>() },
+            { provide: unrespondedRequests, useValue: new Set<number>() },
+            WAV_ENCODER_FACTORY_PROVIDER,
+            WAV_RECODER_FACTORY_PROVIDER,
+            { provide: worker, useValue: new Worker(url) }
+        ]
+    });
 
     const wavEncoderFactory = injector.get(WavEncoderFactory);
 
